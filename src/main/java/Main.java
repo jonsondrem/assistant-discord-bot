1863a260375cf2532e54b972e16e624a49d334ec
jonsondrem <jonsondrem@gmail.com>
2020-06-09 12:46:49 +0200

    replaced token.txt with config.properties. Continued with implementation of bot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,25 @@
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javax.security.auth.login.LoginException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.Properties;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         File file = new File("token.txt");
-        String token;
-        try {
-            Scanner sc = new Scanner(file);
-            sc.useDelimiter("\\Z");
-            token = sc.next();
-            System.out.println(token);
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("Didnt find file");
+        Properties config = loadProperties();
+        String token = config.getProperty("token");
+        if (token.equals("")) {
+            System.out.println("Please write in the token in the config.properties file.");
             return;
         }
         JDABuilder builder = JDABuilder.createDefault(token);
@@ -28,8 +29,65 @@ public class Main extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if(event.getMessage().getContentRaw().equals(".test") && !event.getAuthor().isBot()) {
-            event.getChannel().sendMessage("I managed to read that command :)").queue();
+
+        Member author = event.getMember();
+        assert author != null;
+        String msg = event.getMessage().getContentRaw();
+        String[] command = msg.split(" ");
+
+        //TODO .listen command
+            //TODO add channel to list
+        if(command[0].equals(".listen") && author.getPermissions().contains(Permission.ADMINISTRATOR)) {
+            addChannel(event.getMessage().getChannel());
+        }
+
+        //TODO .unlisten command
+            //TODO remove channel from list
+
+        //TODO check if bot listens to channel
+            //TODO play music command
+    }
+
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+
+        try {
+            FileReader fileReader = new FileReader("config.properties");
+            properties.load(fileReader);
+        } catch (FileNotFoundException e) {
+            properties.setProperty("token", "");
+            properties.setProperty("channels", "");
+            try {
+                FileWriter fileWriter = new FileWriter("config.properties");
+                properties.store(fileWriter, "Config for discord bot.");
+            }
+            catch (IOException s) {
+                s.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+
+    private static void addChannel(MessageChannel channel) {
+        JSONParser jsonParser = new JSONParser();
+        JSONArray channelList;
+
+        JSONObject JSONChannel = new JSONObject();
+        JSONChannel.put("channel", channel);
+
+        try (FileReader fileReader = new FileReader("channels.json")) {
+            Object obj = jsonParser.parse(fileReader);
+
+            channelList = (JSONArray) obj;
+
+            //TODO put channel into JSON Array
+        } catch (FileNotFoundException e) {
+            //TODO generate json file
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
         }
     }
 }
