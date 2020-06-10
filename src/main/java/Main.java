ab0be4c0390ee1b3fd5deb474e2f58b4b37b424e
jonsondrem <jonsondrem@gmail.com>
2020-06-10 21:21:45 +0200

    Unlisten to channel functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,6 @@ import java.util.Properties;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
-        File file = new File("token.txt");
         Properties config = loadProperties();
         String token = config.getProperty("token");
         if (token.equals("")) {
@@ -37,12 +36,16 @@ public class Main extends ListenerAdapter {
 
         if(command[0].equals(".listen") && author.getPermissions().contains(Permission.ADMINISTRATOR)) {
             addChannel(event.getMessage().getChannel());
+            return;
         }
 
-        //TODO .unlisten command
-            //TODO remove channel from list
+        if(command[0].equals(".unlisten") && author.getPermissions().contains(Permission.ADMINISTRATOR)) {
+            removeChannel(event.getMessage().getChannel());
+            return;
+        }
 
         //TODO check if bot listens to channel
+            //TODO list available commands
             //TODO play music command
     }
 
@@ -69,6 +72,7 @@ public class Main extends ListenerAdapter {
         return properties;
     }
 
+    @SuppressWarnings("unchecked")
     private static void addChannel(MessageChannel channel) {
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonChannel = new JSONObject();
@@ -105,5 +109,38 @@ public class Main extends ListenerAdapter {
         }
 
         channel.sendMessage("I have now added this channel to the listening JSON file.").queue();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void removeChannel(MessageChannel channel){
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonChannel = new JSONObject();
+        jsonChannel.put("channel", channel.toString());
+        boolean found = false;
+
+        try (FileReader fileReader = new FileReader("channels.json")) {
+            JSONArray channelList = (JSONArray) jsonParser.parse(fileReader);
+            JSONArray toRemove = new JSONArray();
+            for (Object o : channelList) {
+                JSONObject jsonObj = (JSONObject) o;
+                if (jsonObj.get("channel").equals(jsonChannel.get("channel"))) {
+                    toRemove.add(o);
+                    found = true;
+                }
+            }
+
+            channelList.removeAll(toRemove);
+            FileWriter fileWriter = new FileWriter("channels.json");
+            fileWriter.write(channelList.toJSONString());
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (FileNotFoundException ignored) {
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        if (found) {
+            channel.sendMessage("I am now ignoring this channel.").queue();
+        }
     }
 }
