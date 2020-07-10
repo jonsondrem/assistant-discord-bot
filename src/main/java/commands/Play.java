c94daed25fd0f5b4183498c1c73baf8a41c8ea03
jonsondrem <jonsondrem@gmail.com>
2020-07-10 02:41:38 +0200

    Fixed issue where spotify access token expires + some refactoring

--- src/main/java/commands/Play.java
+++ src/main/java/commands/Play.java
@@ -1,22 +1,12 @@
 package commands;
 
-import api.Spotify;
+import api.SpotifyModel;
 import api.YouTubeModel;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.youtube.model.ResourceId;
-import com.google.api.services.youtube.model.SearchListResponse;
-import com.google.api.services.youtube.model.SearchResult;
 import music.PlayerManager;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
-import com.google.api.services.youtube.YouTube;
-import utilities.PropertiesLoader;
-
-import java.io.IOException;
-import java.util.List;
 
 public class Play extends Command {
 
@@ -53,12 +43,12 @@ public class Play extends Command {
         PlayerManager manager = PlayerManager.getInstance();
         if (command[1].contains("spotify:track:")) {
             String songId = command[1].substring(14);
-            String trackUrl = YouTubeModel.getInstance().searchYouTube(Spotify.getInstance().getSpotifySong(songId));
+            String trackUrl = YouTubeModel.getInstance().searchYouTube(SpotifyModel.getInstance().getSpotifySong(songId));
             manager.loadAndPlay(event.getTextChannel(), trackUrl,
                     event.getMember());
         } else if (command[1].contains("https://open.spotify.com/track/")) {
             String songId = command[1].substring(31).split("\\?")[0];
-            String trackUrl = YouTubeModel.getInstance().searchYouTube(Spotify.getInstance().getSpotifySong(songId));
+            String trackUrl = YouTubeModel.getInstance().searchYouTube(SpotifyModel.getInstance().getSpotifySong(songId));
             manager.loadAndPlay(event.getTextChannel(), trackUrl,
                     event.getMember());
         } else if (command[1].contains("https://")) {
